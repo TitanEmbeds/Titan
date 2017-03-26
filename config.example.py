27c6b7d396c6041d3dc4d7b6561a970dbf7b5a22
Jeremy Zhang <jeremyzhang5000@gmail.com>
2017-03-26 02:30:16 -0700

    Initial configuration example

---
+++ config.example.py
@@ -0,0 +1,13 @@
+config = {
+    # Create an app over here https://discordapp.com/developers/applications/me
+    # and fill these fields out
+    'client-id': "Your app client id",
+    'client-secret': "Your discord client secret",
+    'bot-token': "Discord bot token",
+
+    'app-location': "/var/www/Titan/",
+    'app-secret': "Type something random here, go wild.",
+
+    'database-uri': "driver://username:password@host:port/database",
+    'app-base-url': "The base domain of the site",
+}
