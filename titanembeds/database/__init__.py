71f4785d13aa51b2858f182cf2dd25bb93996661
Jeremy Zhang <jeremyzhang5000@gmail.com>
2017-04-21 18:06:19 -0700

    Initial Redislite implementation with limits

--- titanembeds/database/__init__.py
+++ titanembeds/database/__init__.py
@@ -5,3 +5,4 @@ from guilds import Guilds
 from unauthenticated_users import UnauthenticatedUsers
 from unauthenticated_bans import UnauthenticatedBans
 from authenticated_users import AuthenticatedUsers
+from custom_redislite import LimitsRedisLite
---
+++ titanembeds/database/custom_redislite.py
@@ -0,0 +1,25 @@
+import urlparse
+from limits.storage import Storage
+from redislite import Redis
+
+class LimitsRedisLite(Storage): # For Python Limits
+    STORAGE_SCHEME = "redislite"
+    def __init__(self, uri, **options):
+        self.redis_instance = Redis(urlparse.urlparse(uri).netloc)
+
+    def check(self):
+        return True
+
+    def get_expiry(self, key):
+        return self.redis_instance.ttl(key)
+
+    def incr(self, key, expiry, elastic_expiry=False):
+        if self.redis_instance.exists(key):
+            self.redis_instance.set(key, int(self.redis_instance.get(key))+1)
+        else:
+            self.redis_instance.set(key, 1)
+        self.redis_instance.expire(key, expiry)
+        return
+
+    def get(self, key):
+        return int(self.redis_instance.get(key))
