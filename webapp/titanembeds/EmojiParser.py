fb957783574122427fa966617dd25182740c2b6a
JustMaffie <jorivanee@hotmail.com>
2017-05-29 01:19:35 +0200

    Updated about.html.j2

--- webapp/titanembeds/EmojiParser.py
+++ webapp/titanembeds/EmojiParser.py
@@ -6,12 +6,12 @@ def parseEmoji(textToParse, guild_id):
     _method = "GET"
     response = discord_api.request(_method, _endpoint)
     emojis = []
-    emojis = re.findall(":(.*?):", textToParse)
+    emojis = re.findall("<:(.*?):(.*)?>", textToParse)
     newText = textToParse
     for emoji in response['emojis']:
     	name = emoji['name']
     	emojiId = emoji['id']
     	for emoji2 in emojis:
     		if name.lower is emoji2.replace(":", "").lower():
-    			newText = newText.replace(":{}:".format(name), "<img src='https://cdn.discordapp.com/emojis/{}.png'></img>".format(id))
+    			newText = newText.replace("<:{}}:{}>".format(name, emojiId), "<img src='https://cdn.discordapp.com/emojis/{}.png'></img>".format(emojiId))
     return newText
