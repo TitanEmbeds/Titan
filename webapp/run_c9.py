91384cd1669cf84343c1f9622b490cbcf13666f0
Jeremy Zhang <jkzhang@uw.edu>
2017-05-31 19:28:35 +0000

    Make c9 flask threaded

--- webapp/run_c9.py
+++ webapp/run_c9.py
@@ -3,4 +3,4 @@ import os
 
 if __name__ == "__main__":
     init_debug()
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True, processes=3)
