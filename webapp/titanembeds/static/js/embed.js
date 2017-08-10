98d13e9febcba488d9609468dad96e0231f5ffe3
Jeremy Zhang <jkzhang@uw.edu>
2017-08-10 01:11:18 +0000

    We cant forget the secret way to make italics with underscores

--- webapp/titanembeds/static/js/embed.js
+++ webapp/titanembeds/static/js/embed.js
@@ -608,6 +608,7 @@
         text = text.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
         text = text.replace(/\*(.*?)\*/g, "<i>$1</i>");
         text = text.replace(/__(.*?)__/g, "<u>$1</u>");
+        text = text.replace(/_(.*?)_/g, "<i>$1</i>");
         text = text.replace(/~~(.*?)~~/g, "<del>$1</del>");
         text = text.replace(/\`\`\`([^]+)\`\`\`/g, "<code class=\"blockcode\">$1</code>");
         text = text.replace(/\`(.*?)\`/g, "<code>$1</code>");
