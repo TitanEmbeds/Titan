a2b2cce815244493a82dd4e7a01ee3f9f96b2720
Jeremy Zhang <jkzhang@uw.edu>
2017-11-01 17:30:47 +0000

    Use scroll into view instead

--- webapp/titanembeds/static/js/embed.js
+++ webapp/titanembeds/static/js/embed.js
@@ -663,9 +663,7 @@
         if (selected_channel != channel_id && guild_channels[channel_id] && guild_channels[channel_id].read) {
             if (animate_it) {
                 $("#guild-btn").sideNav("show");
-                if (!element_in_view($("#channel-"+channel_id), true)) {
-                    $("#guild-nav").animate({ scrollTop: $(document).height() }, "fast");
-                }
+                $("#channel-"+channel_id)[0].scrollIntoView({behavior: "smooth"});
                 flashElement($("#channel-"+channel_id));
                 setTimeout(function () {
                     $("#guild-btn").sideNav("hide");
