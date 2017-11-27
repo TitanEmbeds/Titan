87d47a9c1863c9d5277109db04e81d31b2d640d3
Jeremy Zhang <jkzhang@uw.edu>
2017-11-27 02:45:50 +0000

    Implement spanish and turkish langs

--- webapp/titanembeds/i18n.py
+++ webapp/titanembeds/i18n.py
@@ -1,8 +1,10 @@
 LANGUAGES = {
     'da': 'Dansk', # Danish
     'en_US': 'English',
+    'es_ES': 'Español', # Spanish
     'it': 'Italiano', # Italian
     'nl': 'Nederlands', # Dutch
     'pl': 'Polski', # Polish
     'th': "ไทย", # Thai
+    'tr': 'Türk', # Turkish
 }
