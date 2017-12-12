fe6ec7f1aa265a59c0080f2faffdf478db129e61
Jeremy Zhang <jkzhang@uw.edu>
2017-12-12 13:12:38 +0000

    New languages: de fr id pt_PT sr sv_SE

--- webapp/titanembeds/i18n.py
+++ webapp/titanembeds/i18n.py
@@ -1,10 +1,16 @@
 LANGUAGES = {
     'da': 'Dansk', # Danish
+    'de': 'Deutsche', # German
     'en_US': 'English',
     'es_ES': 'Español', # Spanish
+    'fr': 'français', # French
+    'id': 'bahasa Indonesia', # Indonesian
     'it': 'Italiano', # Italian
     'nl': 'Nederlands', # Dutch
     'pl': 'Polski', # Polish
+    'pt_PT': 'Português', # Portuguese
+    'sr': 'Српски', # Serbian (Cyrillic)
+    'sv_SE': 'svenska', # Swedish
     'th': "ไทย", # Thai
     'tr': 'Türk', # Turkish
 }
