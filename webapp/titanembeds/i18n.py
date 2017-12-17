a187b202dbf01bc5e0d8b1d3b65e2b51ea2683e3
Jeremy Zhang <jkzhang@uw.edu>
2017-12-17 22:36:36 +0000

    Update and add translations for hu nl pl ro

--- webapp/titanembeds/i18n.py
+++ webapp/titanembeds/i18n.py
@@ -4,11 +4,13 @@ LANGUAGES = {
     'en_US': 'English',
     'es_ES': 'Español', # Spanish
     'fr': 'français', # French
+    'hu': "Magyar", # Hungarian
     'id': 'bahasa Indonesia', # Indonesian
     'it': 'Italiano', # Italian
     'nl': 'Nederlands', # Dutch
     'pl': 'Polski', # Polish
     'pt_PT': 'Português', # Portuguese
+    'ro': 'Română', # Romanian
     'sr': 'Српски', # Serbian (Cyrillic)
     'sv_SE': 'svenska', # Swedish
     'th': "ไทย", # Thai
