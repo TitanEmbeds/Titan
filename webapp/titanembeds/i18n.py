24120f4981058a6f5a7a57e3b3fa6531b7392028
Jeremy Zhang <jkzhang@uw.edu>
2017-12-30 09:01:04 +0000

    Added languages ca hi zh_CN zh_TW, Updated de

--- webapp/titanembeds/i18n.py
+++ webapp/titanembeds/i18n.py
@@ -1,9 +1,11 @@
 LANGUAGES = {
+    'ca': 'Català', # Catalan
     'da': 'Dansk', # Danish
     'de': 'Deutsche', # German
     'en_US': 'English',
     'es_ES': 'Español', # Spanish
     'fr': 'français', # French
+    'hi': 'हिंदी', # Hindi
     'hu': "Magyar", # Hungarian
     'id': 'bahasa Indonesia', # Indonesian
     'it': 'Italiano', # Italian
@@ -15,4 +17,6 @@ LANGUAGES = {
     'sv_SE': 'svenska', # Swedish
     'th': "ไทย", # Thai
     'tr': 'Türk', # Turkish
+    'zh_CN': '简体中文', # Chinese Simplified
+    'zh_TW': '中国传统的', # Chinese Traditional
 }
