44f44b2a50819700a159313d80ed2b0063202771
Jeremy Zhang <jkzhang@uw.edu>
2018-01-27 23:21:04 +0000

    Unlink messages pot first before generating a new one in translation update

--- webapp/bin/tr_update.py
+++ webapp/bin/tr_update.py
@@ -5,6 +5,9 @@ if sys.platform == 'win32':
     pybabel = 'flask\\Scripts\\pybabel'
 else:
     pybabel = 'pybabel'
+try:
+    os.unlink('titanembeds/translations/messages.pot')
+except:
+    pass
 os.system(pybabel + ' extract -F babel.cfg -k lazy_gettext -o titanembeds/translations/messages.pot titanembeds')
 os.system(pybabel + ' update -i messages.pot -d titanembeds/translations')
-os.unlink('messages.pot')
