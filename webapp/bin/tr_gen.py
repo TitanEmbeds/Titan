7d7437edf48179b49ad423488d4d6fae5935e3dc
Jeremy Zhang <jkzhang@uw.edu>
2018-01-28 00:46:42 +0000

    Generate pot files script

---
+++ webapp/bin/tr_gen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import os
+import sys
+if sys.platform == 'win32':
+    pybabel = 'flask\\Scripts\\pybabel'
+else:
+    pybabel = 'pybabel'
+try:
+    os.unlink('titanembeds/translations/messages.pot')
+except:
+    pass
+os.system(pybabel + ' extract -F babel.cfg -k lazy_gettext -o titanembeds/translations/messages.pot titanembeds')
