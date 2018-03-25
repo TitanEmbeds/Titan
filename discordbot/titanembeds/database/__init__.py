b767df14c91edfa0224ea802ae035f3133bb6708
Jeremy Zhang <jkzhang@uw.edu>
2018-03-25 20:31:25 +0000

    Revert "Wait until got session"
    This reverts commit fa3d533354fb872904a84b42a31173193a3688c9.

--- discordbot/titanembeds/database/__init__.py
+++ discordbot/titanembeds/database/__init__.py
@@ -33,13 +33,7 @@ class DatabaseInterface(object):
 
     @contextmanager
     def get_session(self) -> Session:
-        got_session = False
-        while not got_session:
-            try:
-                session = self._sessionmaker()  # type: Session
-                got_session = True
-            except:
-                pass
+        session = self._sessionmaker()  # type: Session
         try:
             yield session
             session.commit()
