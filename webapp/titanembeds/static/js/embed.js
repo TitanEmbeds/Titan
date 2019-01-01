cb6a87f73ce995922d078a8576d6c9e188c06aac
Jeremy Zhang <jkzhang@uw.edu>
2019-01-01 02:08:15 +0000

    Connect to gateway immediately on login, a test for now

--- webapp/titanembeds/static/js/embed.js
+++ webapp/titanembeds/static/js/embed.js
@@ -54,7 +54,7 @@
     var all_users = []; // List of all the users in guild
     var is_dragging_chatcontainer = false; // Track if is dragging on chatcontainer (does not trigger messagebox focus) or not
     var localstorage_avaliable = false; // Check if localstorage is avaliable on this browser
-    var shouldUtilizeGateway = false; // Don't connect to gateway until page is focused or has interaction.
+    var shouldUtilizeGateway = true; // Don't connect to gateway until page is focused or has interaction. -- set to true for now as an experiment to see how long this lasts...
     var discord_users_list_enabled = false; // Allow automatic population of discord users list
 
     function element_in_view(element, fullyInView) {
